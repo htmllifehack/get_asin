99e0e82fd2dedc2976a91ffde2082727a6e6f7b1
ecolab <htmllifehack@gmail.com>
2020-06-12 17:31:09 +0900

    var => let

--- getAsin.js
+++ getAsin.js
@@ -1,17 +1,17 @@
-var link = document.querySelectorAll('.aok-inline-block.zg-item');
+let link = document.querySelectorAll('.aok-inline-block.zg-item');
 
-var a = [];
+let a = [];
 
 link.forEach(e => a.push(e.querySelector('a').getAttribute('href')));
 
-var NAME = [];
-var ASIN = [];
+let item = [];
+let asin = [];
 
-for(var i=0; i<a.length; i++){
-    NAME.push(decodeURI(a[i].split('/')[1]));
-    ASIN.push(a[i].split('/')[3]);
+for(let i=0; i<a.length; i++){
+    item.push(decodeURI(a[i].split('/')[1]));
+    asin.push(a[i].split('/')[3]);
 }
-for(var i=0; i<ASIN.length; i++){
-    document.write(NAME[i] + '<br>');
-    document.write(ASIN[i] + '<br>');
+for(let i=0; i<asin.length; i++){
+    document.write(item[i] + '<br>');
+    document.write(asin[i] + '<br>');
 }
